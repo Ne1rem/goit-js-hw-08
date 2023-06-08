7635b0de73c230bfd40d9db3bdba406ab606bf14
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-06-08 14:39:44 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const formInput = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input');
+const submitForm = document.querySelector('.submit-form');
+const storageKey = "feedback-form-state";
+const message = document.querySelector('textarea');
+
+submitForm.addEventListener("submit", handleSubmit);
+
+populateTextarea();
+
+formInput.addEventListener('input', throttle(saveInputValue, 500));
+
+function handleSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(storageKey);
+  emailInput.value = '';
+  message.value = '';
+}
+
+function saveInputValue() {
+  const feedback = {
+    email: emailInput.value,
+    message: message.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(feedback));
+}
+
+function populateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(storageKey));
+  
+  if (savedMessage === null) {
+    return;
+  }
+  
+  message.value = savedMessage['message'] || '';
+  emailInput.value = savedMessage['email'] || '';
+}
